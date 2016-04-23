ab89c7db2d90d440304b4293d4419254283dd5d2
Levitah <master_lucas@msn.com>
2016-04-23 15:59:03 -0300

    Commit inicial
    - Commit inicial do projeto;

---
+++ app/src/main/java/br/com/lucassantanna/randomgod/GodDetailActivity.java
@@ -0,0 +1,28 @@
+package br.com.lucassantanna.randomgod;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ScrollView;
+
+public class GodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_god_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton btnTop = (FloatingActionButton) findViewById(R.id.btnTop);
+        btnTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ScrollView)findViewById(R.id.scrollView)).fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
+    }
+}
